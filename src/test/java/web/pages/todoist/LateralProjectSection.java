022377d6359520632202a67470bf754e9912f936
PabloAcker <favw14@gmail.com>
2023-10-20 17:19:17 -0400

    Commit with fila A, B completed final.

--- src/test/java/web/pages/todoist/LateralProjectSection.java
+++ src/test/java/web/pages/todoist/LateralProjectSection.java
@@ -6,7 +6,7 @@ import web.controls.Label;
 public class LateralProjectSection {
     public Label projectSectionLabel = new Label(By.xpath("//a[@href='/app/projects']"));
     public Button addProjectButton = new Button(By.xpath("//button[@aria-label='A\u00f1adir un proyecto']"));
-    public Label checkNewProject(String xpath){
-        return new Label(By.xpath("//span[text()='" + xpath + "'][last()]"));
+    public Label checkNewProject(String name){
+        return new Label(By.xpath("//span[text()='" + name + "'][last()]"));
     }
 }
